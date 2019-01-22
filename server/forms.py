10ba2e7e32fcf3696d52d982fae65968588cd3cd
gauthamzz <thabeatsz@gmail.com>
2019-01-22 17:46:46 +0100

    add testing model

--- server/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import MLModel
-
-class MLModelForm(forms.ModelForm):
-    class Meta:
-        model = MLModel
-        fields = ('title', 'description', 'accuracy', 'file', )
